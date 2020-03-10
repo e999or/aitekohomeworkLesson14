0c6b7d02a6fb83274f05f3cd7da5c4e5728ac50b
Lenovo-PC <e999or@bk.ru>
2020-03-10 22:15:47 +0300

    Lesson 14

--- src/main/java/com/github/e999or/Lesson14/Person.java
+++ src/main/java/com/github/e999or/Lesson14/Person.java
@@ -1,5 +1,28 @@
 package com.github.e999or.Lesson14;
 
+import java.util.List;
+
 public class Person {
-    
+    private String name;
+    private int age;
+    private List<String> doList;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getDolist() {
+        return doList;
+    }
+
+
+    public Person(String name, int age, List<String> dolist) {
+        this.name = name;
+        this.age = age;
+        this.doList = dolist;
+    }
 }
--- src/test/java/com/github/e999or/Lesson14/SimpleTest.java
+++ src/test/java/com/github/e999or/Lesson14/SimpleTest.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 
+import java.util.Arrays;
+import java.util.List;
+//import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class SimpleTest {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleTest.class);
 
@@ -31,4 +37,34 @@ public class SimpleTest {
     public void simpleTest() {
         LOG.info("Simple test");
     }
+
+/*
+    @Test
+    public void chekElements() {
+        List<String> doList = Arrays.asList("Умыться", "Побриться", "Погладить штаны");
+        Person vasya = new Person("Вася", 37, doList);
+        assertThat(vasya)
+                .as("Васян")
+                .hasFieldOrPropertyWithValue("name", "Вася")
+                .hasFieldOrProperty("age");
+        assertThat(vasya.getDolist())
+                .as("Васян")
+                .contains("Умыться")
+                .containsExactly("Умыться", "Побриться", "Погладить штаны")
+                .doesNotContain("Спать");
+    }
+    */
+
+    @Test
+    public void givenList_whenCheck_thenSize3() {
+        List<String> doList = Arrays.asList("Умыться", "Побриться", "Погладить штаны");
+        Person vasya = new Person("Вася", 37, doList);
+        assertThat(doList, hasSize(3));
+        assertThat(doList, contains("Умыться", "Побриться", "Погладить штаны"));
+        assertThat(doList, hasItem("Побриться"));
+    }
+
 }
+
+
+
